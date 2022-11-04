c5976154d17bab9c7073d26cc71dae1d56dc740f
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-04 12:48:34 +0100

    code added to app.js

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ const body = document.body
 const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+  
+    if (activeSlide > slides.length - 1) {
+      activeSlide = 0
+    }
+  
+    setBgToBody()
+    setActiveSlide()
+  })
+  
+  leftBtn.addEventListener('click', () => {
+    activeSlide--
+  
+    if (activeSlide < 0) {
+      activeSlide = slides.length - 1
+    }
+  
+    setBgToBody()
+    setActiveSlide()
+  })
